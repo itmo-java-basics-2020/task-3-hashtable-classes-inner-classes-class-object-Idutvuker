6982fd78adfbe50532b75da6e723ceea2b15166a
Nikolay <smir.nick2000@gmail.com>
2020-03-12 17:36:35 +0300

    Intial commit

--- src/main/java/ru/itmo/java/HashTable.java
+++ src/main/java/ru/itmo/java/HashTable.java
@@ -1,11 +1,48 @@
 package ru.itmo.java;
 
-import java.util.Map;
+public class HashTable
+{
+    private class Pair {
+        public final Object key;
+        public final Object value;
 
-public class HashTable {
+        private Pair(Object key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private Pair[] table;
+    private float loadFactor = 0.5f;
+    private int capacity;
+    private int threshold;
+    private int size = 0;
+
+    private int getHash(Object key) {
+        return key.hashCode() % capacity;
+    }
+
+    HashTable(int initialCapacity) {
+        this.capacity = initialCapacity;
+        threshold = (int) (capacity * loadFactor);
+        table = new Pair[capacity];
+    }
+
+    HashTable(int initialCapacity, float loadFactor) {
+        this.loadFactor = loadFactor;
+        this.capacity = initialCapacity;
+        threshold = (int) (capacity * loadFactor);
+        table = new Pair[capacity];
+    }
 
     Object put(Object key, Object value) {
-        throw new UnsupportedOperationException();
+        int hc = getHash(key);
+        while (table[hc] != null && !table[hc].key.equals(key)) {
+            hc = (hc + 1) % this.capacity;
+        }
+
+        table[hc] = value;
+        size++;
     }
 
     Object get(Object key) {
@@ -17,7 +54,7 @@ public class HashTable {
     }
 
     int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
 }
