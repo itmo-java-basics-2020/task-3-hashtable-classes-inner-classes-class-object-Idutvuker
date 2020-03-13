b1382bbcad564c91894b45991fa5a1342381a3c6
Nikolay <smir.nick2000@gmail.com>
2020-03-13 07:15:19 +0300

    Solve task

--- src/main/java/ru/itmo/java/HashTable.java
+++ src/main/java/ru/itmo/java/HashTable.java
@@ -1,60 +1,132 @@
 package ru.itmo.java;
 
-public class HashTable
-{
-    private class Pair {
-        public final Object key;
-        public final Object value;
+import static java.lang.Math.abs;
 
-        private Pair(Object key, Object value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
+public class HashTable {
+	private class Pair {
+		private final Object key;
+		private final Object value;
+		private boolean deleted = false;
 
-    private Pair[] table;
-    private float loadFactor = 0.5f;
-    private int capacity;
-    private int threshold;
-    private int size = 0;
+		private Pair(Object key, Object value) {
+			this.key = key;
+			this.value = value;
+		}
+	}
 
-    private int getHash(Object key) {
-        return key.hashCode() % capacity;
-    }
+	private Pair[] table;
+	private float loadFactor = 0.5f;
+	private int capacity;
+	private int threshold;
 
-    HashTable(int initialCapacity) {
-        this.capacity = initialCapacity;
-        threshold = (int) (capacity * loadFactor);
-        table = new Pair[capacity];
-    }
+	private int size = 0;
+	private int realSize = 0;
 
-    HashTable(int initialCapacity, float loadFactor) {
-        this.loadFactor = loadFactor;
-        this.capacity = initialCapacity;
-        threshold = (int) (capacity * loadFactor);
-        table = new Pair[capacity];
-    }
 
-    Object put(Object key, Object value) {
-        int hc = getHash(key);
-        while (table[hc] != null && !table[hc].key.equals(key)) {
-            hc = (hc + 1) % this.capacity;
-        }
+	private int getHash(Object key) {
+		return abs(key.hashCode() % capacity);
+	}
 
-        table[hc] = value;
-        size++;
-    }
+	HashTable(int initialCapacity) {
+		this.capacity = initialCapacity;
+		threshold = (int) (capacity * loadFactor);
+		table = new Pair[capacity];
+	}
 
-    Object get(Object key) {
-        throw new UnsupportedOperationException();
-    }
+	HashTable(int initialCapacity, float loadFactor) {
+		this.loadFactor = loadFactor;
+		this.capacity = initialCapacity;
+		threshold = (int) (capacity * loadFactor);
+		table = new Pair[capacity];
+	}
 
-    Object remove(Object key) {
-        throw new UnsupportedOperationException();
-    }
+	private int find(Object key, boolean findDeleted) {
+		int hc = getHash(key);
 
-    int size() {
-        return size;
-    }
+		while (true) {
+			if (table[hc] == null) {
+				break;
+			} else if (table[hc].key.equals(key) && (!table[hc].deleted || findDeleted)) {
+				break;
+			}
+
+			hc = (hc + 1) % this.capacity;
+		}
+
+		return hc;
+	}
+
+
+	private void resize() {
+		Pair[] oldTable = table;
+
+		//System.out.println(realSize + " " + capacity + " " + threshold);
+
+		capacity *= 2;
+		threshold = (int) (capacity * loadFactor);
+		size = 0;
+		realSize = 0;
+
+		table = new Pair[capacity];
+
+		for (Pair pair : oldTable) {
+			if (pair != null && !pair.deleted) {
+				put(pair.key, pair.value);
+			}
+		}
+
+		//System.out.println("New size:" + size +" "+ realSize);
+	}
+
+	Object put(Object key, Object value) {
+		//System.out.println("put "+ key);
+
+		int hc = find(key, true);
+
+		if (table[hc] == null) {
+			table[hc] = new Pair(key, value);
+			size++;
+			realSize++;
+
+			if (realSize == threshold) {
+				resize();
+			}
+
+			return null;
+		} else if (table[hc].deleted) {
+			table[hc] = new Pair(key, value);
+			size++;
+			return null;
+		} else {
+			Object oldVal = table[hc].value;
+			table[hc] = new Pair(key, value);
+
+			return oldVal;
+		}
+	}
+
+	Object get(Object key) {
+		//System.out.println("get " + key);
+		int hc = find(key, false);
+
+		return (table[hc] == null) ? null : table[hc].value;
+	}
+
+	Object remove(Object key) {
+		//System.out.println("remove " + key);
+		int hc = find(key, false);
+
+		if (table[hc] != null) {
+			table[hc].deleted = true;
+			size--;
+			return table[hc].value;
+		}
+
+		return null;
+	}
+
+	int size() {
+		return size;
+	}
 
 }
